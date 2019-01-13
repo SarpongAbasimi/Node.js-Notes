d3f58484c9c05f12005883e04d5d8ae456bc20ec
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-13 15:38:33 +0000

    Update Express.js

--- Express.js
+++ Express.js
@@ -45,7 +45,10 @@ app.engine('handlebars',handlebars({defaultLayout :'layout',extensionName:'handl
 app.set('view engine', 'handlebar')
 
 
-
+//Express.Router
+//An Express router provides a subset of Express methods. 
+//To create an instance of one, we invoke the .Router() method on the top-level Express import.
+//To use a router, we mount it at a certain path using app.use() and pass in the router as the second argument. 
 
 
 
