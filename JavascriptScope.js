113a631a5763d2603b06003327a58bbe6d4b1a1d
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-15 13:01:36 +0000

    Create JavascriptScope.js

---
+++ JavascriptScope.js
@@ -0,0 +1,8 @@
+//Blocks and Scope
+//A block is the code found inside a set of curly braces {}.
+//Blocks help us group one or more statements together and serve as an important structural marker for our code.
+
+const logSkyColor = () => {
+  let color = 'blue'; 
+  console.log(color); // blue 
+};
