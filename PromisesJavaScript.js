fafddf5b1f7444f1036e6581367fc14e46f28c9e
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-16 17:25:14 +0000

    Update PromisesJavaScript.js

--- PromisesJavaScript.js
+++ PromisesJavaScript.js
@@ -167,14 +167,14 @@ myAction();
 1)Promises are JavaScript objects that represent the eventual result of an asynchronous operation.
 2)Promises can be in one of three states: pending, resolved, or rejected.
 3)A promise is settled if it is either resolved or rejected 
-We construct a promise by using the new keyword and passing an executor function to the Promise constructor method.
-setTimeout() is a Node function which delays the execution of a callback function using the event-loop.
-We use .then() with a success handler callback containing the logic for what should happen if a promise resolves.
-use .catch() with a failure handler callback containing the logic for what should happen if a promise rejects.
-Promise composition enables us to write complex, asynchronous code that's still readable. We do this by chaining multiple .then()'s and .catch()'s.
-To use promise composition correctly, we have to remember to return promises constructed within a .then().
-We should chain multiple promises rather than nesting them.
-To take advantage of concurrency, we can use Promise.all().
+4)We construct a promise by using the new keyword and passing an executor function to the Promise constructor method.
+5)setTimeout() is a Node function which delays the execution of a callback function using the event-loop.
+6)We use .then() with a success handler callback containing the logic for what should happen if a promise resolves.
+7)use .catch() with a failure handler callback containing the logic for what should happen if a promise rejects.
+8)Promise composition enables us to write complex, asynchronous code that's still readable. We do this by chaining multiple .then()'s and .catch()'s.
+9)To use promise composition correctly, we have to remember to return promises constructed within a .then().
+10)We should chain multiple promises rather than nesting them.
+11)To take advantage of concurrency, we can use Promise.all().
 
 
 */
