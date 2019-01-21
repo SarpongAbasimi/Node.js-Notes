6a389634f6649c60c66e728f9abb6ea3989ab288
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-21 14:57:40 +0000

    Update React.js

--- React.js
+++ React.js
@@ -73,6 +73,12 @@ Note.
 
 */
 
+//*********************************************************** IMPORTANT NOTE
+/*  
+A React component should use props to store information that can be changed, but can only be changed by a different component.
+A React component should use state to store information that the component itself can change.
+*/
+
 
 
 
