daf942dba8665897bfa83a1ed4f5cf14b4347d26
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-25 17:27:51 +0000

    Update TestingWithMocha.js

--- TestingWithMocha.js
+++ TestingWithMocha.js
@@ -1,1 +1,30 @@
 //How To Use Mocha
+
+// Install mocha with npm npm install --save-dev mocha
+
+const myString = (input)=>{
+    let myArray= []
+     
+  return myArray.concat(input).length;
+}
+
+module.exports = myString;
+
+
+//In a second file
+//Mocha allows you to use any assertion library
+//Assertion chai , expect.js,better-assert , should.js
+
+const assert = require('assert');
+const app = require('./f');
+
+
+describe('App',function(){
+    const myArray = ['hello',5,7,8];
+    it('Must return the lenght of an array',function(){
+            assert.equal(app(myArray),5);
+    })
+})
+
+
+
