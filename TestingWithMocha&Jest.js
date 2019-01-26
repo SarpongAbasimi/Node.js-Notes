f496d08b8847ad48c68b67ccdb33c7ef1f5307eb
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-26 01:14:18 +0000

    Update and rename TestingWithMocha.js to TestingWithMocha&Jest.js

--- TestingWithMocha&Jest.js
+++ TestingWithMocha&Jest.js
@@ -1,5 +1,4 @@
 //How To Use Mocha
-
 // Install mocha with npm npm install --save-dev mocha
 
 const myString = (input)=>{
@@ -7,9 +6,7 @@ const myString = (input)=>{
      
   return myArray.concat(input).length;
 }
-
 module.exports = myString;
-
 
 //In a second file
 //Mocha allows you to use any assertion library
@@ -18,7 +15,6 @@ module.exports = myString;
 const assert = require('assert');
 const app = require('./f');
 
-
 describe('App',function(){
     const myArray = ['hello',5,7,8];
     it('Must return the lenght of an array',function(){
@@ -26,5 +22,26 @@ describe('App',function(){
     })
 })
 
+//Testing Promise with Jest 
+
+//uppercase.js
+const uppercase = str => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject('Empty string')
+      return
+    }
+    resolve(str.toUpperCase())
+  })
+}
+module.exports = uppercase
+
+//uppercase.test.js
+const uppercase = require('./uppercase')
+test(`uppercase 'test' to equal 'TEST'`, () => {
+  return uppercase('test').then(str => {
+    expect(str).toBe('TEST')
+  })
+})
 
 
