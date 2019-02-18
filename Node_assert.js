5ec30c44c58597b7ef1f33cf6f303ab54b87184c
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-18 20:39:08 +0000

    Create Node_assert.js

---
+++ Node_assert.js
@@ -0,0 +1,11 @@
+/*
+assert.ok
+As a Node module, assert can be imported at the top of your files with
+The functions in the assert library compare values and throw errors as needed using one function call. The small, human-readable format of the functions will help you make a more expressive test suite.
+assert.ok(6 - 1 === 5);
+
+In this case 6 - 1 === 5 evaluates to true, so no error is thrown.
+If an argument passed to assert.ok() evaluates to false, an AssertionError is thrown. The error communicates to Mocha that a test has failed, and Mocha logs the error message to the console.
+
+*/
+const assert = require('assert')
