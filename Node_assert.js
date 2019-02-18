ac6903dc49a0e43f34811d42dde689144a8dfe75
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-18 20:39:41 +0000

    Update Node_assert.js

--- Node_assert.js
+++ Node_assert.js
@@ -5,7 +5,7 @@ The functions in the assert library compare values and throw errors as needed us
 assert.ok(6 - 1 === 5);
 
 In this case 6 - 1 === 5 evaluates to true, so no error is thrown.
-If an argument passed to assert.ok() evaluates to false, an AssertionError is thrown. The error communicates to Mocha that a test has failed, and Mocha logs the error message to the console.
+If an argument passed to assert.ok() evaluates to false, an AssertionError is thrown. The error communicates to Mocha that a test has failed, and Mocha logs the error message to the console
 
 */
 const assert = require('assert')
