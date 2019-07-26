b2d816ca43236c00a5c98798e6b8bfba60823b87
sarpong <abasimisarpong@rocketmail.com>
2019-07-26 19:39:52 +0100

    changed route to point to static dir

--- src/server.js
+++ src/server.js
@@ -6,15 +6,40 @@ path = require('path');
 
 const server = http.createServer((req, res)=> {
   const filePath = path.join(__dirname, '../public/templates/index.html')
-  res.writeHead(200, {contentType: 'text/html'})
-  fs.readFile(filePath , (err, filedata)=> {
-    if(err){
-      res.write(err)
-    }
-    res.write(filedata)
-    res.end()
-  });
-})
+  let requestUrl = req.url;
+  switch (requestUrl) {
+    case '/':
+      fs.readFile(filePath , (err, filedata)=> {
+      if(err){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('sorry there was an error')
+        res.end()
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.write(filedata)
+      res.end()
+    });    
+      break;
+      
+    case '/static/index.css':
+      fs.readFile(path.join(__dirname, '../public/static/index.css'), (err, data)=>{
+        if(err){
+          res.writeHead(200, {'Content-Type': 'text/css'})
+          res.write('sorry ther was an error')
+          res.end()
+        }else {
+          res.writeHead(200, {'Content-Type': 'text/css'})
+          res.write(data)
+          res.end()
+        }
+      });
+      break;
+    default:
+      res.write('I have nothing for this endpoint')
+      res.end()
+      break;
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
